254eea9a9d12f193ea6f2f6d4ccf6a323d2de80e
ralonso <rodrigo.gtr@gmail.com>
2022-01-21 18:41:24 -0300

    :sparkles: Basic POST request and data saving

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Basic Flask API
 
-## 2. Basic Routing
+## 3. Basic POST request and database saving
 
 ### Local setup
 
@@ -8,6 +8,13 @@
 - Create a virtual environment with called 'env' with: python3 -m env env
 - Activate virtual environment with: source env/bin/activate
 - Download dependencies with: pip install -r requirements.txt
-- Run the server with: python3 src/main.py
-- In the browser, go to: localhost:5000
-- You should see "{ "msg": "Hello!"}". You have a basic API!
+- Create basic sqlite database: enter the python interpreter with: python3
+- Type: from src.main import db
+- Type: db.create_all()  (this will create a db file with the columns specified in the defined User model)
+- Exit with: exit()
+
+#### Add user
+
+- run the server with: python3 src/main.py
+- Make a POST request (using Postman) to http://localhost:5000/user 
+- The server should respond with the saved user
--- src/main.py
+++ src/main.py
@@ -1,12 +1,59 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
 
 # Init app
 app = Flask(__name__)
+base_dir = os.path.abspath(os.path.dirname(__file__)) #getting the directory of the current file
 
-# Defining main route
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({"msg": "Hello!"})
+# Configuring a basic Sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "my_db.sqlite")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize db Sqlite
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow, object serialization/de-serialization library)
+ma = Marshmallow(app)
+
+# Using SQLALchemy ORM; create a model to interact with the database:
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    occupation = db.Column(db.String(200))
+    income = db.Column(db.Float)
+    password = db.Column(db.String(200)) #TODO: password must be hashed before saving!
+
+    def __init__(self, name, occupation, income, password):
+        self.name = name
+        self.occupation = occupation
+        self.income = income
+        password = password
+
+#Define a schema with Marshmallow 
+class UserSchema(ma.Schema):
+    class Meta: #define the fields that are allowed to be shown
+        fields = ('id', 'name', 'occupation', 'income')
+
+user_schema = UserSchema()
+users_Schema = UserSchema(many=True)
+
+# Defining routes and methods for the API
+@app.route('/user', methods=['POST'])
+def add_user():
+    name = request.json['name']
+    occupation = request.json['occupation']
+    income = request.json['income']
+    password = request.json['password']
+
+    #Save new user
+    new_user = User(name, occupation, income, password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user)
 
 # RunServer
 if __name__ == '__main__':
